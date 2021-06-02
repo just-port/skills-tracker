cd88ff0e16fefc169581b8dc66bd2a6d77b14b7f
Justin Portell <72322829+just-port@users.noreply.github.com>
2021-06-02 09:43:23 -0500

    initial commit

---
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -0,0 +1,27 @@
+package org.launchcode.skillstracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SkillsController {
+
+    @GetMapping
+    @ResponseBody
+    public String mainPage() {
+        return "<html>" +
+                "<body>" +
+                "<h1>Skills Tracker</h1>" +
+                "<h2>We have a few skills we would like to learn. Here is the list!</h2>" +
+                "<ol>" +
+                    "<li>Java</li>" +
+                    "<li></li>" +
+                    "<li></li>" +
+                    "<li></li>" +
+                "<ol/>" +
+                "</body>" +
+                "</html>";
+    }
+
+}
